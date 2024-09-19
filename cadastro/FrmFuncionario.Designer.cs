f331c2f88c427b96ecb7dd1852f3cc3a0f85ba63
Thiago dos Passos Lima <166858263+Thichi99@users.noreply.github.com>
2024-09-19 17:16:21 -0300

    criando branch cadastroFuncionario

--- cadastro/FrmFuncionario.Designer.cs
+++ cadastro/FrmFuncionario.Designer.cs
@@ -36,6 +36,22 @@ namespace PDV.cadastro
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.cbCargo = new System.Windows.Forms.ComboBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.img = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -58,15 +74,15 @@ namespace PDV.cadastro
             // 
             this.label3.Location = new System.Drawing.Point(12, 64);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.Size = new System.Drawing.Size(37, 16);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Endereço:";
+            this.label3.Text = "Email:";
             // 
             // label4
             // 
-            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Location = new System.Drawing.Point(416, 60);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 20);
+            this.label4.Size = new System.Drawing.Size(44, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Cargo:";
             // 
@@ -78,11 +94,140 @@ namespace PDV.cadastro
             this.label5.TabIndex = 4;
             this.label5.Text = "Foto:";
             // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(12, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Telefone:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(74, 5);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(313, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(74, 31);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(144, 20);
+            this.txtTel.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(74, 60);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(313, 20);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(12, 91);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Endereço:";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(74, 86);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(313, 20);
+            this.txtEndereco.TabIndex = 10;
+            // 
+            // cbCargo
+            // 
+            this.cbCargo.FormattingEnabled = true;
+            this.cbCargo.Items.AddRange(new object[] { "Gerente", "Atendente" });
+            this.cbCargo.Location = new System.Drawing.Point(459, 57);
+            this.cbCargo.Name = "cbCargo";
+            this.cbCargo.Size = new System.Drawing.Size(113, 21);
+            this.cbCargo.TabIndex = 11;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(457, 5);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(145, 20);
+            this.txtCpf.TabIndex = 12;
+            // 
+            // img
+            // 
+            this.img.Location = new System.Drawing.Point(658, 5);
+            this.img.Name = "img";
+            this.img.Size = new System.Drawing.Size(129, 162);
+            this.img.TabIndex = 13;
+            this.img.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(774, 192);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(14, 387);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 15;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(95, 387);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "Salvar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(176, 387);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 17;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(257, 387);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 18;
+            this.btnDelete.Text = "Deletar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
             // FrmFuncionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 422);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.img);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.cbCargo);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -92,8 +237,34 @@ namespace PDV.cadastro
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro Funcionários";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+
+        private System.Windows.Forms.Button btnNovo;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.PictureBox img;
+
+        private System.Windows.Forms.ComboBox cbCargo;
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtEndereco;
+
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.TextBox txtEmail;
+
+        private System.Windows.Forms.TextBox txtNome;
+
+        private System.Windows.Forms.Label label6;
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
